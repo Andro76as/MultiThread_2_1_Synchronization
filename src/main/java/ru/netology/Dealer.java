ff9834491a00b101c957525858b1fed992fb9438
Andro76as <andro76as@gmail.com>
2022-06-27 19:33:57 +0300

    Commit3

--- src/main/java/ru/netology/Dealer.java
+++ src/main/java/ru/netology/Dealer.java
@@ -4,16 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dealer {
-    private static final int carCount = 7;
-    private static final int recieveTime = 1000;
-    private static final int sellTime = 1000;
+    private static final int CAR_COUNT = 7;
+    private static final int RECIEVE_TIME = 1000;
+    private static final int SELL_TIME = 1000;
     private final List<Car> cars = new ArrayList<>();
 
     public void receiveCar() {
-        for (int i = 0; i < carCount; i++) {
+        for (int i = 0; i < CAR_COUNT; i++) {
             try {
                 System.out.println("Производитель toyota выпустил 1 авто");
-                Thread.sleep(recieveTime);
+                Thread.sleep(RECIEVE_TIME);
                 synchronized (this) {
                     cars.add(new Car());
                     notify();
@@ -31,7 +31,7 @@ public class Dealer {
                 System.out.println("Машин нет");
                 wait();
             }
-            Thread.sleep(sellTime);
+            Thread.sleep(SELL_TIME);
             System.out.println("Покупатель " + Thread.currentThread().getName() + " уехал на новеньком авто");
 
         } catch (InterruptedException e) {
